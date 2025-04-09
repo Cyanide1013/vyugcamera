c1f122f38d0d74aa91c63380d02d361bb05e7a2b
Cyanide1013 <87561494+Cyanide1013@users.noreply.github.com>
2025-04-09 15:06:35 +0530

    changed lens

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./index.css";
 
 (async function () {
   const cameraKit = await bootstrapCameraKit({
-    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzMyNzc3OTIzLCJzdWIiOiI5YzRkNjMxNy1iYmI5LTRjODQtOWYzMC0wZTVmMzA3NDUxZGZ-U1RBR0lOR34zYjY4MTBkYy0xN2Y2LTRmNzctYTNiZS02YTdjMjU0Zjc2YjgifQ.La2HprjDjaVTHUAEgc7hvsk2xKx7krdqfvuPwdXzN9o',
+    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzMyNzc3NDg1LCJzdWIiOiIwNDBlYThiNy0xMzk4LTRkOTktOThjMi1iZjUzN2ZmYmE5NDZ-U1RBR0lOR343NjEyMzgyOS0wYzQxLTQ2NWMtYjE3OC1iYjJlZWY4YTNlYTIifQ.GAm5xphhO0p9Jb5LESxzI9dOtTWwCA4lg78phW_Ks88',
   });
   const liveRenderTarget = document.getElementById(
     'canvas'
@@ -30,9 +30,8 @@ import "./index.css";
   // }, 1000);
 
   const lens = await cameraKit.lensRepository.loadLens(
-    'f0c77a23-a231-4206-9d7a-1008bc4c9629',
-    '67e7b4ff-7878-46fd-b778-adbf25e9a722'
-  );
+    'e27e2101-5fa9-4c5a-a991-6cea672231d8',
+    '259776e5-d370-4529-98d4-a25d80c77dd0'  );
 
   await session.applyLens(lens);
 })();
